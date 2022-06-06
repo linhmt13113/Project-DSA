d57d9e37593ebee1f8fc6564311d0a19d69f3e37
Linh <linh.pp269@gmail.com>
2022-06-06 09:28:43 +0700

    game

---
+++ lines98-master/PlayGame.java
@@ -0,0 +1,13 @@
+
+//================================================================
+import Lines.*;
+
+public class PlayGame{
+	
+	public static void main(String[] args)throws Exception{	
+				
+        LineFrame app = new LineFrame();
+        app.startGame();           
+         	
+	}
+}
